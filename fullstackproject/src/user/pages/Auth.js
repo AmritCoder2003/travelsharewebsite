9dcbc0ee73fefa910c3793edca642db8c067c797
AMRIT CHATTOPADHYAY <159451583+AmritCoder2003@users.noreply.github.com>
2024-09-22 13:32:06 +0530

    Update Auth.js

--- fullstackproject/src/user/pages/Auth.js
+++ fullstackproject/src/user/pages/Auth.js
@@ -70,7 +70,7 @@ const Auth = () => {
     console.log(formState.inputs);
     if (isLoginMode) {
       try {
-        const response = await fetch('http://localhost:5000/api/users/login', {
+        const response = await fetch('https://travelsharewebsite.onrender.com/api/users/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
