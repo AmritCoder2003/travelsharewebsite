58054382093feadc1fdfe359f32180ba882f27d2
AMRIT CHATTOPADHYAY <159451583+AmritCoder2003@users.noreply.github.com>
2024-09-22 13:33:08 +0530

    Update Users.js

--- fullstackproject/src/user/pages/Users.js
+++ fullstackproject/src/user/pages/Users.js
@@ -10,7 +10,7 @@ const Users =() => {
     const sendRequest=async()=>{
       setIsLoading(true);
       try{
-        const response=await fetch('http://localhost:5000/api/users');
+        const response=await fetch('https://travelsharewebsite.onrender.com/api/users');
         const responseData=await response.json();
         if(!response.ok){
           throw new Error(responseData.message);
